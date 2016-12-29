d2f04de35ee8ad1efd2d35369d3dc501e3321c9a
Lu Hao-Jui <andy810914@gmail.com>
2016-12-29 20:56:11 +0800

    add some attribute to User and A new node Location

--- application/dating_app/website/models.py
+++ application/dating_app/website/models.py
@@ -1,11 +1,45 @@
-from django.db import models
+from neomodel import (StructuredNode, StringProperty, IntegerProperty, RelationshipFrom)
 from django.utils import timezone
 
 
-class User(models.Model):
-    username = models.CharField(max_length=30)
-    password = models.CharField(max_length=30)
-    email = models.EmailField()
 
-    def __str__(self):
-        return self.username
+
+class User(StructuredNode):
+        # basic info
+        username = StringProperty(max_length=30)
+        password = StringProperty(max_length=30)
+        email = StringProperty()
+
+        # details info
+        picture = StringProperty()
+        orientation = StringProperty()
+        user_url = StringProperty()
+        gender = StringProperty()   # 1 for male, 0 for female
+        age = IntegerProperty()    # how old is this user
+        rel_status = StringProperty()      # single or open relationship
+        location = StringProperty()
+        id = StringProperty()
+        entertainement = StringProperty()
+        we_behavior = StringProperty()
+        essentials = StringProperty()
+        summary = StringProperty()
+        thinkings = StringProperty()
+        messaging_conditions = StringProperty()
+        #looking_for
+
+
+        # Relationship
+        following = RelationshipFrom('User', 'FOLLOWING')
+        at_location = RelationshipFrom('Location', 'AT')
+
+        def __str__(self):
+                return self.username
+
+class Location(StructuredNode):
+        location_name = StringProperty()
+
+        near = RelationshipFrom('Location', 'NEAR')
+        inhabitant = RelationshipFrom('User', 'IS_FROM')
+
+        def __str__(self):
+                return self.location_name
